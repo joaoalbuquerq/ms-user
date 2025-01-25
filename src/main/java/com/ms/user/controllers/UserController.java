191300fa04c13bfd320b2de5dca0c9ee6faa6eed
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2025-01-24 23:54:37 -0300

    adicionando fluxo final de desenvolvimento do endpoint post de cadastro de usuários

--- src/main/java/com/ms/user/controllers/UserController.java
+++ src/main/java/com/ms/user/controllers/UserController.java
@@ -2,7 +2,10 @@ package com.ms.user.controllers;
 
 import com.ms.user.dtos.UserRecordDto;
 import com.ms.user.models.UserModel;
+import com.ms.user.service.UserService;
 import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,9 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
     @PostMapping
     public ResponseEntity<UserModel> cadastrarUsuario(@RequestBody @Valid UserRecordDto dto){
-        return null;
+
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(dto,userModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.cadastrar(userModel));
     }
 
 }
