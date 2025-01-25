e13ccd8fd30ada60ab1a0b74b8892e20bb64428f
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2025-01-25 16:45:48 -0300

    adicionando ajustes no servico de cadastro de usuários

--- src/main/java/com/ms/user/service/UserService.java
+++ src/main/java/com/ms/user/service/UserService.java
@@ -1,6 +1,7 @@
 package com.ms.user.service;
 
 import com.ms.user.models.UserModel;
+import com.ms.user.producers.UserProducer;
 import com.ms.user.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
@@ -9,14 +10,18 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     final UserRepository userRepository;
+    final UserProducer userProducer;
 
-    public UserService(UserRepository userRepository){
+    public UserService(UserRepository userRepository, UserProducer userProducer){
         this.userRepository = userRepository;
+        this.userProducer = userProducer;
     }
 
     @Transactional
     public UserModel cadastrar(UserModel userModel){
-        return userRepository.save(userModel);
+        userModel = userRepository.save(userModel);
+        userProducer.publicarMensagemNaFila(userModel);
+        return userModel;
     }
 
 }
